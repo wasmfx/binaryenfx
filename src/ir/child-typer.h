38b4c2ba827eb186af1ecebb983cd65bb0c6fa95
Daniel Hillerström <daniel.hillerstrom@ed.ac.uk>
2024-10-10 21:08:27 +0200

    Off by one error, fix

--- src/ir/child-typer.h
+++ src/ir/child-typer.h
@@ -1096,8 +1096,8 @@ template<typename Subtype> struct ChildTyper : OverriddenVisitor<Subtype> {
 
   void visitStackSwitch(StackSwitch* curr) {
     auto params = curr->contType.getContinuation().type.getSignature().params;
-    assert(params.size() == curr->operands.size());
-    for (size_t i = 0; i < params.size(); ++i) {
+    assert(params.size() >= 1 && ((params.size() - 1) == curr->operands.size()));
+    for (size_t i = 0; i < params.size() - 1; ++i) {
       note(&curr->operands[i], params[i]);
     }
     note(&curr->cont, Type(curr->contType, Nullable));
